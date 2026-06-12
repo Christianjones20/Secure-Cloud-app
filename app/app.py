c4c8b720e1f8f85d5a31b6869d572026d840cbe1
Chris <Christianjjones20@gmail.com>
2026-06-12 17:31:44 -0400

    Update Flask App

--- app/app.py
+++ app/app.py
@@ -1,32 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
 APP_VERSION = "1.0.0"
-ENVIROMENT = "Production"
+ENVIRONMENT = os.getenv("APP_ENV", "Production")
 
 @app.route("/")
 def home():
-    return {
-            "message": "Cloud Status Dashboard",
-            "status": "running",
-            "enviroment": ENVIROMENT,
-            "server_time": datetime.utcnow().isoformat() + "Z"
-    }
+    return render_template(
+        "index.html",
+        app_version=APP_VERSION,
+        environment=ENVIRONMENT,
+        server_time=datetime.utcnow().isoformat() + "Z"
+    )
 
 @app.route("/version")
 def version():
-        return {
-            "version": APP_VERSION
-}
+    return {
+        "version": APP_VERSION
+    }
 
 @app.route("/health")
 def health():
-        return {
-            "status": "healthy",
-            "message": "Application is running successfully"
-}
+    return {
+        "status": "healthy",
+        "message": "Application is running successfully",
+        "environment": ENVIRONMENT
+    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
